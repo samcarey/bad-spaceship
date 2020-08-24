be5dd433e8620a9a46290c72b9ca6809b66945e1
Sam Carey <sam@samcarey.com>
2020-08-23 21:12:27 -0600

    factored hello into plugin

--- bevy/src/main.rs
+++ bevy/src/main.rs
@@ -2,14 +2,9 @@ use bevy::prelude::*;
 
 fn main() {
     App::build()
-        .add_startup_system(add_people.system())
-        .add_system(hello_world.system())
-        .add_system(greet_people.system())
+        .add_default_plugins()
+        .add_plugin(HelloPlugin)
         .run();
-}
-
-fn hello_world() {
-    println!("hello world!");
 }
 
 struct Person;
@@ -23,6 +18,15 @@ fn add_people(mut commands: Commands) {
         .spawn((Person, Name("Zayna Nieves".to_string())));
 }
 
-fn greet_people(person: &Person, name: &Name) {
+fn greet_people(_person: &Person, name: &Name) {
     println!("hello {}!", name.0);
+}
+
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut AppBuilder) {
+        app.add_startup_system(add_people.system())
+            .add_system(greet_people.system());
+    }
 }
