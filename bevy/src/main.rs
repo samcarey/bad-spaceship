7d9258299949ac845ae2729f759e173b0c09f897
Sam Carey <sam@samcarey.com>
2020-08-23 21:26:53 -0600

    Added timer

--- bevy/src/main.rs
+++ bevy/src/main.rs
@@ -18,15 +18,25 @@ fn add_people(mut commands: Commands) {
         .spawn((Person, Name("Zayna Nieves".to_string())));
 }
 
-fn greet_people(_person: &Person, name: &Name) {
-    println!("hello {}!", name.0);
+struct GreetTimer(Timer);
+
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, _person: &Person, name: &Name) {
+    // update our timer with the time elapsed since the last update
+    timer.0.tick(time.delta_seconds);
+
+    // check to see if the timer has finished. if it has, print our message
+    if timer.0.finished {
+        println!("hello {}!", name.0);
+    }
 }
 
 pub struct HelloPlugin;
 
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut AppBuilder) {
-        app.add_startup_system(add_people.system())
+        // the reason we call from_seconds with the true flag is to make the timer repeat itself
+        app.add_resource(GreetTimer(Timer::from_seconds(2.0, true)))
+            .add_startup_system(add_people.system())
             .add_system(greet_people.system());
     }
 }
