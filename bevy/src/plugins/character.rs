436ac9a228b31bc98d2ce679a2e3a106f31fe9f6
Sam Carey <sam@samcarey.com>
2020-09-01 06:42:38 -0600

    moved constants to config file

--- bevy/src/plugins/character.rs
+++ bevy/src/plugins/character.rs
@@ -1,4 +1,6 @@
 use bevy::prelude::*;
+use serde::Deserialize;
+use std::fs;
 
 pub struct CharacterPlugin;
 
@@ -8,15 +10,9 @@ impl Plugin for CharacterPlugin {
     }
 }
 
+const CONFIG_FILE: &str = "assets/config/character.ron";
+
 const RADIANS_IN_CIRCLE: f32 = 2.0 * std::f32::consts::PI;
-
-const SIZE: f32 = 1.5;
-const HOVER_SIZE_RATIO: f32 = 0.2;
-
-const BOB_RATIO: f32 = 0.15;
-const BOB_RATE: f32 = 1.7;
-
-const EXTRA_FIRST_PERSON_HEIGHT_SIZE_RATIO: f32 = 0.5;
 
 struct Name(String);
 
@@ -28,17 +24,36 @@ struct Bob {
 
 pub struct BasePosition(pub Vec3);
 
+#[derive(Deserialize)]
+struct Config {
+    size: f32,
+    hover_size_ratio: f32,
+    bob_ratio: f32,
+    bob_rate: f32,
+    extra_first_person_height_size_ratio: f32,
+    name: String,
+}
+
+impl Config {
+    fn new() -> Self {
+        let config_string = fs::read_to_string(CONFIG_FILE).unwrap();
+        ron::from_str(&config_string[..]).unwrap()
+    }
+}
+
 pub fn spawn(
     commands: &mut Commands,
     meshes: &mut ResMut<Assets<Mesh>>,
     materials: &mut ResMut<Assets<StandardMaterial>>,
 ) -> (Option<Entity>, f32) {
-    let hover = SIZE * HOVER_SIZE_RATIO;
-    let base_height = SIZE / 2.0 + hover;
-    let static_height_of_top = SIZE + hover;
-    let bob_amplitude = hover * BOB_RATIO;
+    let config = Config::new();
+
+    let hover = config.size * config.hover_size_ratio;
+    let base_height = config.size / 2.0 + hover;
+    let static_height_of_top = config.size + hover;
+    let bob_amplitude = hover * config.bob_ratio;
     let dynamic_height_of_top = static_height_of_top + bob_amplitude;
-    let extra_first_person_height = EXTRA_FIRST_PERSON_HEIGHT_SIZE_RATIO * SIZE;
+    let extra_first_person_height = config.extra_first_person_height_size_ratio * config.size;
     let first_person_camera_height = dynamic_height_of_top * extra_first_person_height;
 
     let base_position = BasePosition(Vec3::new(
@@ -53,17 +68,19 @@ pub fn spawn(
     });
     let character_entity = commands
         .spawn(PbrComponents {
-            mesh: meshes.add(Mesh::from(shape::Cube { size: SIZE / 2.0 })),
+            mesh: meshes.add(Mesh::from(shape::Cube {
+                size: config.size / 2.0,
+            })),
             material: cube_mat_handle,
             translation: Translation(base_position.0),
             ..Default::default()
         })
         .with(base_position)
-        .with(Name("Name".to_string()))
+        .with(Name(config.name))
         .with(Bob {
             amplitude: bob_amplitude,
             phase: 0.0,
-            radians_per_second: BOB_RATE * RADIANS_IN_CIRCLE,
+            radians_per_second: config.bob_rate * RADIANS_IN_CIRCLE,
         })
         .current_entity();
     (character_entity, first_person_camera_height)
