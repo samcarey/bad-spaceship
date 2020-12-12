537159bf2e5d1e3ca7fee5e2084f0cabd88ec947
Sam Carey <sam@samcarey.com>
2020-12-12 14:44:03 -0700

    start adding menu functionality

--- bevy/src/main.rs
+++ bevy/src/main.rs
@@ -15,5 +15,6 @@ fn main() {
         .add_plugin(RapierRenderPlugin)
         .add_plugin(plugins::MapPlugin)
         .add_plugin(plugins::PlayerPlugin)
+        .add_plugin(plugins::UiPlugin)
         .run();
 }
--- bevy/src/plugins/mod.rs
+++ bevy/src/plugins/mod.rs
@@ -1,7 +1,9 @@
 pub use self::character::CharacterPlugin;
 pub use self::map::MapPlugin;
 pub use self::player::PlayerPlugin;
+pub use self::ui::UiPlugin;
 
 mod character;
 mod map;
 mod player;
+mod ui;
