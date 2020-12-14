dac65595de69dfdf8cbaaaba9afbdd870414749c
Sam Carey <sam@samcarey.com>
2020-12-13 20:45:18 -0700

    displays 3 centered buttons

--- bevy/src/plugins/ui.rs
+++ bevy/src/plugins/ui.rs
@@ -1,5 +1,17 @@
 use bevy::prelude::*;
 use bevy::ui::prelude::ButtonComponents;
+use config_from_file_macro::ConfigFromFileMacro;
+use config_from_file_macro_derive::ConfigFromFileMacro;
+use serde::Deserialize;
+
+const CONFIG_FILE: &str = "assets/config/ui.ron";
+
+#[derive(ConfigFromFileMacro, Deserialize)]
+struct Config {
+    menu_button_height: f32,
+    menu_button_width: f32,
+    menu_button_spacing: f32,
+}
 
 pub struct UiPlugin;
 
@@ -75,37 +87,83 @@ fn button_system(
 fn setup(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
     button_materials: Res<ButtonMaterials>,
 ) {
+    let config: Config = Config::new(CONFIG_FILE);
+    let names = ["Options", "Multiplayer", "Resume"];
+    let offset_increment = config.menu_button_height + config.menu_button_spacing;
+    let menu_items_height = config.menu_button_height * names.len() as f32
+        + config.menu_button_spacing * (names.len() - 1) as f32;
+
     commands
         // ui camera
         .spawn(UiCameraComponents::default())
-        .spawn(ButtonComponents {
+        // root node
+        .spawn(NodeComponents {
             style: Style {
-                size: Size::new(Val::Px(150.0), Val::Px(65.0)),
-                // center button
-                margin: Rect::all(Val::Auto),
-                // horizontally center child text
+                size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
                 justify_content: JustifyContent::Center,
-                // vertically center child text
                 align_items: AlignItems::Center,
                 ..Default::default()
             },
-            material: button_materials.normal.clone(),
+            material: materials.add(Color::NONE.into()),
             ..Default::default()
         })
         .with_children(|parent| {
-            parent.spawn(TextComponents {
-                text: Text {
-                    value: "Button".to_string(),
-                    font: asset_server.load("fonts/FiraSans-Bold.ttf"),
-                    style: TextStyle {
-                        font_size: 40.0,
-                        color: Color::rgb(0.9, 0.9, 0.9),
+            parent
+                // menu node
+                .spawn(NodeComponents {
+                    style: Style {
+                        size: Size::new(
+                            Val::Px(config.menu_button_width),
+                            Val::Px(menu_items_height),
+                        ),
+                        justify_content: JustifyContent::Center,
+                        align_items: AlignItems::FlexEnd,
+                        ..Default::default()
                     },
-                },
-                ..Default::default()
-            });
+                    material: materials.add(Color::NONE.into()),
+                    ..Default::default()
+                })
+                .with_children(|parent| {
+                    // buttons
+                    for (i, name) in names.iter().enumerate() {
+                        parent
+                            .spawn(ButtonComponents {
+                                style: Style {
+                                    size: Size::new(Val::Percent(100.0), Val::Px(65.0)),
+                                    // center button
+                                    margin: Rect::all(Val::Auto),
+                                    // horizontally center child text
+                                    justify_content: JustifyContent::Center,
+                                    // vertically center child text
+                                    align_items: AlignItems::Center,
+                                    position_type: PositionType::Absolute,
+                                    position: Rect {
+                                        top: Val::Px(i as f32 * offset_increment),
+                                        ..Default::default()
+                                    },
+                                    ..Default::default()
+                                },
+                                material: button_materials.normal.clone(),
+                                ..Default::default()
+                            })
+                            .with_children(|parent| {
+                                parent.spawn(TextComponents {
+                                    text: Text {
+                                        value: name.to_string(),
+                                        font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                                        style: TextStyle {
+                                            font_size: 40.0,
+                                            color: Color::rgb(0.9, 0.9, 0.9),
+                                        },
+                                    },
+                                    ..Default::default()
+                                });
+                            });
+                    }
+                });
         });
 }
 
