1ecc6ae280ac90e3fb56a5936bead918cd1d5f37
Sam Carey <sam@samcarey.com>
2020-12-15 13:14:52 -0700

    use for-each

--- bevy/src/plugins/ui.rs
+++ bevy/src/plugins/ui.rs
@@ -57,19 +57,19 @@ impl FromResources for ButtonMaterials {
 
 fn button_highlight(
     button_materials: Res<ButtonMaterials>,
-    mut interaction_query: Query<(&Button, Mutated<Interaction>, &mut Handle<ColorMaterial>)>,
+    _button: &Button,
+    interaction: Mutated<Interaction>,
+    mut material: Mut<Handle<ColorMaterial>>,
 ) {
-    for (_button, interaction, mut material) in interaction_query.iter_mut() {
-        match *interaction {
-            Interaction::Clicked => {
-                *material = button_materials.pressed.clone();
-            }
-            Interaction::Hovered => {
-                *material = button_materials.hovered.clone();
-            }
-            Interaction::None => {
-                *material = button_materials.normal.clone();
-            }
+    match *interaction {
+        Interaction::Clicked => {
+            *material = button_materials.pressed.clone();
+        }
+        Interaction::Hovered => {
+            *material = button_materials.hovered.clone();
+        }
+        Interaction::None => {
+            *material = button_materials.normal.clone();
         }
     }
 }
