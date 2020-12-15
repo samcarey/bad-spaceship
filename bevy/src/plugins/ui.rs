16da86b2538fab7f7405da5866453430bf193101
Sam Carey <sam@samcarey.com>
2020-12-15 13:17:18 -0700

    tweak color

--- bevy/src/plugins/ui.rs
+++ bevy/src/plugins/ui.rs
@@ -50,7 +50,7 @@ impl FromResources for ButtonMaterials {
         ButtonMaterials {
             normal: materials.add(Color::rgb(0.15, 0.15, 0.15).into()),
             hovered: materials.add(Color::rgb(0.25, 0.25, 0.25).into()),
-            pressed: materials.add(Color::rgb(0.35, 0.75, 0.35).into()),
+            pressed: materials.add(Color::rgb(0.35, 0.35, 0.35).into()),
         }
     }
 }
