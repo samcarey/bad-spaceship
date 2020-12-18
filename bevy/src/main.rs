d3dfb7c52c6d22e42fc4a665780b458c150572fc
Sam Carey <sam@samcarey.com>
2020-12-17 21:53:58 -0700

    removed most plugins from server config

--- bevy/src/main.rs
+++ bevy/src/main.rs
@@ -10,25 +10,38 @@ fn main() {
         .expect("A logger was already initialized");
     let args = utils::parse_args();
 
-    App::build()
-        .add_resource(args)
-        .add_resource(ClearColor(Color::rgb(
-            0xF9 as f32 / 255.0,
-            0xF9 as f32 / 255.0,
-            0xFF as f32 / 255.0,
-        )))
-        .add_plugins(DefaultPlugins)
-        .add_plugin(RapierPhysicsPlugin)
-        .add_plugin(RapierRenderPlugin)
-        .add_plugin(plugins::MapPlugin)
-        .add_plugin(plugins::PlayerPlugin)
-        .add_plugin(plugins::UiPlugin)
+    let mut app = App::build();
+
+    if args.is_server {
+        app.add_plugins(MinimalPlugins);
+    } else {
+        app.add_plugins(DefaultPlugins)
+            .add_plugin(plugins::UiPlugin)
+            .add_plugin(RapierRenderPlugin)
+            .add_resource(ClearColor(Color::rgb(
+                0xF9 as f32 / 255.0,
+                0xF9 as f32 / 255.0,
+                0xFF as f32 / 255.0,
+            )))
+            .add_plugin(RapierPhysicsPlugin)
+            .add_plugin(plugins::MapPlugin)
+            .add_plugin(plugins::PlayerPlugin);
+    }
+
+    app.add_resource(args)
         .add_plugins_with(plugins::MultiplayerPlugins, |group| {
             if args.is_server {
                 group.disable::<plugins::ClientPlugin>()
             } else {
                 group.disable::<plugins::ServerPlugin>()
             }
-        })
-        .run();
+        });
+
+    app.run();
 }
+
+// let args = utils::parse_args();
+
+// let mut app = App::build();
+
+// app.run();
