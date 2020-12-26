277cef3640443fff662ddd5a68aeecb6d1b09ca6
Sam Carey <sam@samcarey.com>
2020-12-26 12:40:06 -0700

    fix trivial bug

--- bevy/src/utils.rs
+++ bevy/src/utils.rs
@@ -4,52 +4,61 @@ pub struct Args {
 }
 
 #[cfg(target_arch = "wasm32")]
-pub fn parse_args() -> Args {
-    Args { is_server: false }
-}
+#[macro_use]
+mod platform {
+    use super::Args;
 
-#[cfg(not(target_arch = "wasm32"))]
-pub fn parse_args() -> Args {
-    Args {
-        is_server: std::env::args().any(|arg| ["--server", "-s"].contains(&&*arg)),
+    pub fn parse_args() -> Args {
+        Args { is_server: false }
+    }
+
+    #[macro_export]
+    macro_rules! config_from_file {
+        ($filepath: literal) => {
+            ron::from_str(
+                &crate::CONFIG_DIR
+                    .get_file($filepath)
+                    .unwrap()
+                    .contents_utf8()
+                    .unwrap()[..],
+            )
+            .unwrap()
+        };
+    }
+
+    pub mod html_body {
+        use web_sys::HtmlElement;
+
+        pub fn get() -> HtmlElement {
+            let window = web_sys::window().expect("no global `window` exists");
+            let document = window.document().expect("should have a document on window");
+            let body = document.body().expect("document should have a body");
+            body
+        }
     }
 }
 
-#[cfg(target_arch = "wasm32")]
-#[macro_export]
-macro_rules! config_from_file {
-    ($filepath: literal) => {
-        ron::from_str(
-            &crate::CONFIG_DIR
-                .get_file($filepath)
-                .unwrap()
-                .contents_utf8()
-                .unwrap()[..],
-        )
-        .unwrap()
-    };
-}
-
 #[cfg(not(target_arch = "wasm32"))]
-#[macro_export]
-macro_rules! config_from_file {
-    ($filepath: literal) => {
-        ron::from_str(
-            &std::fs::read_to_string(std::path::Path::new("assets/config").join($filepath))
-                .unwrap()[..],
-        )
-        .unwrap()
-    };
-}
+#[macro_use]
+mod platform {
+    use super::Args;
 
-#[cfg(target_arch = "wasm32")]
-pub mod html_body {
-    use web_sys::HtmlElement;
+    pub fn parse_args() -> Args {
+        Args {
+            is_server: std::env::args().any(|arg| ["--server", "-s"].contains(&&*arg)),
+        }
+    }
 
-    pub fn get() -> HtmlElement {
-        let window = web_sys::window().expect("no global `window` exists");
-        let document = window.document().expect("should have a document on window");
-        let body = document.body().expect("document should have a body");
-        body
+    #[macro_export]
+    macro_rules! config_from_file {
+        ($filepath: literal) => {
+            ron::from_str(
+                &std::fs::read_to_string(std::path::Path::new("assets/config").join($filepath))
+                    .unwrap()[..],
+            )
+            .unwrap()
+        };
     }
 }
+
+pub use platform::*;
