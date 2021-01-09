8b4979896c715e073ff8052e856885ea282bb755
Sam Carey <sam@samcarey.com>
2021-01-09 13:22:44 -0700

    factor geometry extension traits

--- bevy/src/utils.rs
+++ bevy/src/utils.rs
@@ -62,3 +62,60 @@ mod platform {
 }
 
 pub use platform::*;
+
+use bevy::prelude::*;
+use rapier3d::math::{Translation, Vector};
+
+pub trait Vec3Ext {
+    fn to_vector(&self) -> Vector<f32>;
+    fn to_translation3(&self) -> Translation<f32>;
+}
+
+impl Vec3Ext for Vec3 {
+    fn to_vector(&self) -> Vector<f32> {
+        Vector::new(self.x, self.y, self.z)
+    }
+
+    fn to_translation3(&self) -> Translation<f32> {
+        Translation::new(self.x, self.y, self.z)
+    }
+}
+
+// TODO: submit this to bevy project
+pub trait TransformExt {
+    fn right(&self) -> Vec3;
+    fn up(&self) -> Vec3;
+}
+
+impl TransformExt for Transform {
+    fn right(&self) -> Vec3 {
+        self.rotation * Vec3::unit_x()
+    }
+
+    fn up(&self) -> Vec3 {
+        self.rotation * Vec3::unit_y()
+    }
+}
+
+use nalgebra::Quaternion;
+
+pub trait QuatExt {
+    fn to(&self, other: Quat) -> Quat;
+    fn to_rotation_vector(&self) -> Vector<f32>;
+    fn to_quaternion(&self) -> Quaternion<f32>;
+}
+
+impl QuatExt for Quat {
+    fn to(&self, other: Quat) -> Quat {
+        (self.conjugate() * other).normalize()
+    }
+
+    fn to_rotation_vector(&self) -> Vector<f32> {
+        let (axis, angle) = self.to_axis_angle();
+        axis.to_vector() * angle
+    }
+
+    fn to_quaternion(&self) -> Quaternion<f32> {
+        Quaternion::new(self.w, self.x, self.y, self.z)
+    }
+}
