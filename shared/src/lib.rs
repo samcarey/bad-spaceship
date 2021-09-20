1bd5263e2447d818349de359d2d46fc333e7ec96
Sam Carey <sam@samcarey.com>
2021-09-19 19:31:06 -0600

    remove empty tests

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -12,14 +12,6 @@ pub mod part;
 pub mod player;
 pub mod utils;
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
-
 #[derive(Default)]
 pub struct KeyboardDirectionalInput(pub Vec3);
 
